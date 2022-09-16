b747a43c80c7f4eb2c5366f3ebdfd964df7d4e20
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2022-09-15 17:00:18 -0700

    Fixed issue when the bookId is actually passed as an event

--- src/components/CompendiumBrowser.vue
+++ src/components/CompendiumBrowser.vue
@@ -57,7 +57,8 @@ export default {
             }
             game.user.setFlag("forge-compendium-browser", "last-book", bookId);
           }
-        } else {
+        } else if(typeof bookId == "string") {
+          ui.notifications.warn("You don't have access to this compendium book.");
           this.book = null;
         }
       } else {
